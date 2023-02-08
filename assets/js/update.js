120e4bf4b9a042a455bc85f9e4e199a9631a289c
KellyDenisse <kellydenissemendoza@gmail.com>
2023-02-08 17:29:51 -0500

    BOM, DOM y Debugging

--- assets/js/update.js
+++ assets/js/update.js
@@ -1,13 +1,26 @@
 
 let runScripts = () => {
     let chart_bars = document.getElementById("chart-bars")
-    console.log(chart_bars)
+    console.log(chart_bars);
 
     let icons = document.getElementsByTagName("i")
-    console.log(icons)
+    console.log(icons);
 
     let cards = document.getElementsByClassName("card")
-    console.log(cards)
+    console.log(cards);
 } 
 
 runScripts ();
+
+let modifyText = () => {
+    let listOfP = document.getElementsByTagName('p');
+    let elementP = listOfP[3];
+    elementP.innerHTML = 'Dinero Actual';
+
+    let listOfF4 = document.getElementsByTagName('h4');
+    let elementH4 = listOfF4[0];
+    elementH4.innerHTML = '$301k';
+
+}
+
+modifyText()
