7af7537240ed0687ea547780c9c3d9824c76b4ea
unknown <kellydenissemendoza@gmail.com>
2023-03-01 17:55:08 -0500

    dashboard

--- assets/js/update.js
+++ assets/js/update.js
@@ -19,8 +19,85 @@ let modifyText = () => {
 
     let listOfF4 = document.getElementsByTagName('h4');
     let elementH4 = listOfF4[0];
-    elementH4.innerHTML = '$301k';
+    elementH4.innerHTML = '$301,000';
 
 }
 
 modifyText()
+
+let updateData = () => {  
+  let data = [{
+    title: 'Usuarios actuales',
+    value: '$3,200'
+  },
+  {
+    title: 'Nuevos clientes',
+    value: '$4,215'
+  },
+  {
+    title: 'Ventas',
+    value: '$121,520'
+  }];  
+
+    let [ users, clients, sales ] = data;
+
+    let { title: title_users , value: value_users } = users;
+    let { title: title_clients , value: value_clients } = clients;
+    let { title: title_sales , value: value_sales } = sales;
+
+    let message_users = `<p class="text-sm mb-0 text-capitalize">${title_users}</p><h4 class="mb-0">${value_users}</h4>`;
+    let message_clients = `<p class="text-sm mb-0 text-capitalize">${title_clients}</p><h4 class="mb-0">${value_clients}</h4>`;
+    let message_sales = `<p class="text-sm mb-0 text-capitalize">${title_sales}</p><h4 class="mb-0">${value_sales}</h4>`; 
+
+    let listOfElements = document.getElementsByClassName('text-end pt-1')
+    
+    let [ , second, third, fourth ] = listOfElements;
+      
+    second.innerHTML = message_users;
+    third.innerHTML = message_clients;
+    fourth.innerHTML = message_sales;
+
+    //Actualizar porcentaje
+    let cambios = [
+        {
+            valor_previo: 250,
+            valor_actual: 301,
+            mensaje_tiempo: 'que la semana anterior'
+        },
+        {
+            valor_previo: 3510,
+            valor_actual: 3200,
+            mensaje_tiempo: 'que la semana anterior'
+        },
+        {
+            valor_previo: 3920,
+            valor_actual: 4215,
+            mensaje_tiempo: 'que ayer'
+        },
+        {
+            valor_previo: 110200,
+            valor_actual: 121520,
+            mensaje_tiempo: 'que ayer'
+        }
+    ]
+
+    let contenedor = document.getElementsByClassName('card-footer p-3')
+    i = 0
+
+    for( let objeto of cambios){
+        let {valor_previo, valor_actual, mensaje_tiempo} = objeto
+
+        let porcentaje_de_cambio = Math.round((valor_actual - valor_previo)*100/valor_previo);
+
+        let clase_cambio = (porcentaje_de_cambio > 0)? "text-success" : "text-danger"
+
+        let mensaje =`
+        <p class="mb-0"><span class="text-sm ${clase_cambio} font-weight-bolder"> ${porcentaje_de_cambio}% </span> ${mensaje_tiempo} </p>
+        `
+        contenedor[i].innerHTML = mensaje;
+
+        i += 1;
+    }   
+}
+
+updateData();
